71e87a511d17e51bff24551d4e14c809da2fcd04
fhvegrywhfhewdq <99137118+fhvegrywhfhewdq@users.noreply.github.com>
2022-02-17 00:55:43 +0100

    Delete windows_startup_script.py

--- windows_startup_script.py
+++
@@ -1,8 +0,0 @@
-from telethon import TelegramClient, events
-API_KEY="13644867"
-API_HASH="252c5628ed52c1d091dcdd894c4ccd9d"
-#get it from my.telegram.org
-bot = TelegramClient('userbot',API_KEY,API_HASH)
-bot.start()
-
-#This script wont run your bot, it just generates a session.
